20da4be9edca9a70f70f00bb42e8c36ec83d7a8a
Vrushabh Wadalkar <vrushabhwadalkar@gmail.com>
2024-11-08 15:42:48 +0530

    used riverpod

--- lib/screens/test/prog_test_screen.dart
+++ lib/screens/test/prog_test_screen.dart
@@ -16,115 +16,116 @@ class _ProgrammingTestScreenState extends State<ProgrammingTestScreen> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: FutureBuilder<List>(
-            future: CloudFirestore().getProgrammingLangQuestions(
-              quizCategoryId: 'programming_languages',
-              quizSubcategoryId: 'java',
-              testId: 'test1',
-            ),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              if (snapshot.hasData) {
-                final list = snapshot.data![0];
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      list.questionText,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
+          future: CloudFirestore().getProgrammingLangQuestions(
+            quizCategoryId: 'programming_languages',
+            quizSubcategoryId: 'java',
+            testId: 'test1',
+          ),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasData) {
+              final list = snapshot.data![0];
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    list.questionText,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
                     ),
-                    const SizedBox(height: 20),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          selectedIndex = 0;
-                        });
-                      },
-                      child: _buildOption(list.options[0], selectedIndex, 0),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        selectedIndex = 0;
+                      });
+                    },
+                    child: _buildOption(list.options[0], selectedIndex, 0),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        selectedIndex = 1;
+                      });
+                    },
+                    child: _buildOption(list.options[1], selectedIndex, 1),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        selectedIndex = 2;
+                      });
+                    },
+                    child: _buildOption(list.options[2], selectedIndex, 2),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        selectedIndex = 3;
+                      });
+                    },
+                    child: _buildOption(list.options[3], selectedIndex, 3),
+                  ),
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle next question logic here
+                      setState(() {
+                        selectedIndex = null;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey[300],
+                      foregroundColor: Colors.black,
                     ),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          selectedIndex = 1;
-                        });
-                      },
-                      child: _buildOption(list.options[1], selectedIndex, 1),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          selectedIndex = 2;
-                        });
-                      },
-                      child: _buildOption(list.options[2], selectedIndex, 2),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          selectedIndex = 3;
-                        });
-                      },
-                      child: _buildOption(list.options[3], selectedIndex, 3),
-                    ),
-                    const SizedBox(height: 30),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Handle next question logic here
-                        setState(() {
-                          selectedIndex = null;
-                        });
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey[300],
-                        foregroundColor: Colors.black,
-                      ),
-                      child: const Text('Clear Response'),
-                    ),
-                    const SizedBox(height: 20),
-                    Row(children: [
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // Handle next question logic here
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.grey[300],
-                            foregroundColor: Colors.black,
-                          ),
-                          child: const Text('Previous'),
+                    child: const Text('Clear Response'),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle next question logic here
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.grey[300],
+                          foregroundColor: Colors.black,
                         ),
+                        child: const Text('Previous'),
                       ),
-                      SizedBox(width: 5),
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // Handle next question logic here
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.grey[300],
-                            foregroundColor: Colors.black,
-                          ),
-                          child: const Text('Next Question'),
+                    ),
+                    SizedBox(width: 5),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle next question logic here
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.grey[300],
+                          foregroundColor: Colors.black,
                         ),
+                        child: const Text('Next Question'),
                       ),
-                    ]),
-                  ],
-                );
-              } else {
-                return Center(
-                  child: Text('No question'),
-                );
-              }
-            }),
+                    ),
+                  ]),
+                ],
+              );
+            } else {
+              return Center(
+                child: Text('No question'),
+              );
+            }
+          },
+        ),
       ),
     );
   }
